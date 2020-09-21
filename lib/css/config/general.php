f000e58ae8bda7e36f37d1e8fed7e77d0f2bd0b4
Matthias Bathke <matthias-bathke@straightvisions.com>
2020-09-21 19:35:40 +0200

    MIGRATE TO V5.100

--- lib/css/config/general.php
+++ lib/css/config/general.php
@@ -2,8 +2,8 @@
 	echo $_s->build_css(
 		is_admin() ? '.editor-styles-wrapper .wp-block-search' : '.sv100_sv_content_wrapper article .wp-block-search',
 		array_merge(
-			$script->get_parent()->get_setting('padding')->get_css_data('padding'),
-			$script->get_parent()->get_setting('margin')->get_css_data(),
-			$script->get_parent()->get_setting('border')->get_css_data()
+			$module->get_setting('padding')->get_css_data('padding'),
+			$module->get_setting('margin')->get_css_data(),
+			$module->get_setting('border')->get_css_data()
 		)
 	);
